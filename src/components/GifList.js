982770b437fd91a1813b90c63e89e39bcb0f9363
Danny Torres <dtorres@teravisiontech.com>
2018-04-26 16:45:43 -0500

    Adding a modal to the images

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -3,7 +3,9 @@ import GiftItem from './GifItem';
 
 const GifList = (props) => {
     const gifItems = props.gifs.map((image) => {
-        return <GiftItem key={image.id} gif={image} />
+        return <GiftItem key={image.id}
+                         gif={image}
+                         onGifSelect={props.onGifSelect}/>
     });
     return (
         <div className="gif-list">{gifItems}</div>
